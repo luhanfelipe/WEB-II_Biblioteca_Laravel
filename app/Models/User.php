8d186f390123bf5f4bda44369c99844b60f712e4
Luhan Felipe <luhanfelipe28@gmail.com>
2026-06-25 23:26:47 -0300

    Atividade 11 - Commit 1/4: Preparar ambiente da API
    - Instalado Laravel Sanctum via php artisan install:api
    - Criado arquivo routes/api.php
    - Adicionado trait HasApiTokens ao model User
    - Preparação para criação da API REST do recurso Book

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Book;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
 
     // Constante com os papéis disponíveis
     public const ROLES = [
